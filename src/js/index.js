184f450b025883701c83b21476b145b25d7f92a0
Vitor Martins <vitor.martins@fever.pt>
2019-08-05 14:49:16 +0100

    ADD Babel.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 // Global app controller
 import x from './test';
-console.log(`I imported ${x} from another file`)
+const i = 23;
+console.log(`I imported ${x} from another file! Variable i is ${i}`)
