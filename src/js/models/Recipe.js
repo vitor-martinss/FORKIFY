67c05a2a502e35da7f8048a423f5e08bf09c2b62
Vitor Martins <vitor.martins@fever.pt>
2019-08-06 15:13:10 +0100

    UPD Serving buttons working

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { key, proxy} from '../config';
+import { inflate } from 'zlib';
 
 export default class Recipe{
     constructor(id){
@@ -89,5 +90,15 @@ export default class Recipe{
             return objIng;
         });
         this.ingredients = newIngredients;
+    }
+
+    updateServings (type) {
+        // Servings
+        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+        // Ingredients
+        this.ingredients.forEach(ing => {
+            ing.count *= (newServings / this.servings)
+        })
+        this.servings = newServings;
     }
 }
